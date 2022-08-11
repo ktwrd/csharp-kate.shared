f1a1ccaa0683d6872168ffc1c246669156323a39
kate <kate@dariox.club>
2022-08-11 13:16:50 +0800

    Add field JSON

--- kate.shared/Helpers/RegExStatements.cs
+++ kate.shared/Helpers/RegExStatements.cs
@@ -10,5 +10,6 @@ namespace kate.shared.Helpers
         public static Regex Base64 = new Regex(@"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$");
         public static Regex Username = new Regex(@"^[0-9A-Za-z_\-]{3,}$");
         public static Regex Password = new Regex(@"^[0-9A-Za-z_\-]{8,}$");
+        public static Regex JSON = new Regex(@"{[^}]+}");
     }
 }
