3ee2e2e0fd53d7342fc578c40a5000d8c941f25d
kate <kate@dariox.club>
2024-04-22 14:49:53 +0800

    Updated StringExtension.cs

--- kate.shared/Extensions/StringExtension.cs
+++ kate.shared/Extensions/StringExtension.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace kate.shared.Extensions
 {
+    /// <summary>
+    /// Extensions for strings.
+    /// </summary>
     public static class StringExtension
     {
-        public static string[] Split(this string value, string seperator)
+        /// <summary>
+        /// Split a string by a separator.
+        /// </summary>
+        /// <param name="value">String</param>
+        /// <param name="separator">Separator.</param>
+        /// <returns>Splitted string.</returns>
+        public static string[] Split(this string value, string separator)
         {
-            return value.Split(value.ToCharArray());
+            return value.Split(separator.ToCharArray());
+        }
+        /// <summary>
+        /// Get the value of the <see cref="DescriptionAttribute"/> on an object, if it is on it.
+        /// </summary>
+        /// <returns>Empty string when no <see cref="DescriptionAttribute"/> found</returns>
+        public static string ToDescriptionString<T>(this T value) where T : struct
+        {
+            if (value.ToString() == null)
+                return "";
+            var attributes = (DescriptionAttribute[])value
+               .GetType()
+               .GetField(value.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes?.Length > 0 ? attributes[0].Description : string.Empty;
         }
     }
 }
