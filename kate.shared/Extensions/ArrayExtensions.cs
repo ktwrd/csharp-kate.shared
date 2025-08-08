383c3f629373882b7de13128fa730fcf58787b54
kate <kate@dariox.club>
2025-08-08 14:43:48 +0800

    Add copyright/Apache 2.0 license to all files.

--- kate.shared/Extensions/ArrayExtensions.cs
+++ kate.shared/Extensions/ArrayExtensions.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+   Copyright 2022-2025 Kate Ward <kate@dariox.club>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -18,9 +34,9 @@ namespace kate.shared.Extensions
             if (oldIndex == newIndex)
                 return;
             if (newIndex >= array.Length)
-                throw new ArgumentOutOfRangeException($"{nameof(newIndex)} does not exist in this array");
+                throw new ArgumentOutOfRangeException(nameof(newIndex), $"Value does not exist in this array");
             if (oldIndex >= array.Length)
-                throw new ArgumentOutOfRangeException($"{nameof(oldIndex)} does not exist in this array");
+                throw new ArgumentOutOfRangeException(nameof(oldIndex), $"Value does not exist in this array");
 
             var tmpArray = new T[array.Length];
             array.CopyTo(tmpArray, 0);
--- kate.shared/Helpers/RandomHelper.cs
+++ kate.shared/Helpers/RandomHelper.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+   Copyright 2022-2025 Kate Ward <kate@dariox.club>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,11 +75,11 @@ namespace kate.shared.Helpers
         /// <summary>
         /// Value to use
         /// </summary>
-        public T Value;
+        public T Value { get; set; }
         /// <summary>
         /// Percentage chance for this item being picked. (0.0f to 1.0f)
         /// </summary>
-        public double Chance;
+        public double Chance { get; set; }
         /// <summary>
         /// Create an instance of this.
         /// </summary>
@@ -73,7 +89,7 @@ namespace kate.shared.Helpers
         public RandomChanceItem(T value, double chance)
         {
             if (chance < 0.0d || chance > 1.0d)
-                throw new ArgumentOutOfRangeException("chance must be between 0f and 1f");
+                throw new ArgumentOutOfRangeException(nameof(chance), "Must be between 0f and 1f");
             Value = value;
             Chance = chance;
         }
