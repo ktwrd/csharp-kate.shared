668f3efe79b6aedce2a92f85a112960ec1134588
kate <kate@dariox.club>
2026-06-23 12:08:06 +0800

    kate.shared: Updated comments in ExceptionHelper

--- kate.shared/Helpers/ExceptionHelper.cs
+++ kate.shared/Helpers/ExceptionHelper.cs
@@ -1,12 +1,19 @@
-///////////////////////////////////////////////////////////////////////////////
-//   Copyright 2026 Kate Ward (https://kate.pet)                             //
-//                                                                           //
-//   Licensed under the Apache License, Version 2.0 (the "License");         //
-//   you may not use this file except in compliance with the License.        //
-//   You may obtain a copy of the License at                                 //
-//                                                                           //
-//       http://www.apache.org/licenses/LICENSE-2.0                          //
-///////////////////////////////////////////////////////////////////////////////
+/*
+   Copyright 2022-2025 Kate Ward <kate@dariox.club>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -187,8 +194,6 @@ public static class ExceptionHelper
     /// <summary>
     /// Calls <see cref="IsTimedOut(Exception, bool, bool, bool)"/>, while setting all the options to <see langword="true"/>
     /// </summary>
-    /// <param name="exception"></param>
-    /// <returns></returns>
     public static bool IsTimedOut(Exception exception)
     {
         return IsTimedOut(exception, true, true, true);
@@ -248,6 +253,9 @@ public static class ExceptionHelper
         }
     }
 
+    /// <exception cref="ArgumentException">
+    /// Thrown when an item with a key matching <paramref name="guid"/> already exists.
+    /// </exception>
     public static void AddTimedOutExpression(Guid guid, Regex expression)
     {
         IsTimedOutExpressionsLock.Wait();
